463edc8ba07c969f84342c089cc1b6ef054b97a1
payal-s-1 <M1089106@mindtree.com>
2022-07-01 01:38:57 +0530

    modified the first file

--- selenium1.java
+++ selenium1.java
@@ -16,7 +16,7 @@ public class selenium1 {
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//input[@class='gNO89b']")).click();
 		
-		// adding a comment for github test
+		// modifying the first folder
 	}
 
 }
